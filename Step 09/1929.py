2e117a27391fdfe64c6e3980f001525e99253f88
isieun0908 <supiya0908@naver.com>
2022-02-17 03:58:36 +0900

    step 09 1929-소수 구하기

--- Step 09/1929.py
+++ Step 09/1929.py
@@ -1,7 +1,21 @@
 def main():
     M, N = map(int, input().split())
 
+    prime = {}
 
+    for i in range(M, N+1):
+        prime[i] = 1
+    prime[1] = 0
+
+    for i in range(2, N):
+        loop = 2
+        while i*loop <= N:
+            prime[i*loop] = 0
+            loop += 1
+
+    for primeNum in prime.keys():
+        if prime[primeNum]:
+            print(primeNum)
 
 if __name__ == "__main__":
     main()
