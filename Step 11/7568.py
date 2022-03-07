c8d7388023fae7e5cc8c089b4291b73543a76fc1
isieun0908 <supiya0908@naver.com>
2022-03-07 23:55:49 +0900

    step 11 7568-덩치

---
+++ Step 11/7568.py
@@ -0,0 +1,19 @@
+def main():
+    N = int(input())
+
+    dungchi = []
+    for i in range(0, N):
+        w, h = map(int, input().split())
+        dungchi.append([])
+        dungchi[i].append(w)
+        dungchi[i].append(h)
+
+    for i in range(0, N):
+        count = 1
+        for j in range(0, N):
+            if dungchi[i][0] < dungchi[j][0] and dungchi[i][1] < dungchi[j][1]:
+                count += 1
+        print(count, end=' ')
+
+if __name__ == "__main__":
+    main()
