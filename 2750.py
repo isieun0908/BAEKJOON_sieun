63b81f386a5867de6a05a8c1252d385e25892089
isieun0908 <supiya0908@naver.com>
2022-03-11 02:50:22 +0900

    step 12 2750-수 정렬하기(insertion sort+bubble sort)

--- 2750.py
+++ 2750.py
@@ -13,9 +13,26 @@ def insertion_sort(N):
     for i in range(0, N):
         print(list[i])
 
+def bubble_sort(N):
+    list = []
+    for i in range(0, N):
+        list.append(int(input()))
+    flag = 1
+    while flag:
+        flag = 0
+        for i in range(0,N-1):
+            if list[i] > list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
+                flag = 1
+    for i in range(0, N):
+        print(list[i])
+
 def main():
     N = int(input())
-    insertion_sort(N)
+    #insertion_sort(N)
+    bubble_sort(N)
 
 if __name__ == "__main__":
     main()
