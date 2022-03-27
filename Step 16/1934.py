692f8bed6f839663373e9f99e0de1bd98171c730
Sieun <supiya0908@naver.com>
2022-03-28 05:31:21 +0900

    step 16 1934-최소공배수

---
+++ Step 16/1934.py
@@ -0,0 +1,17 @@
+def main():
+    T = int(input())
+
+    for i in range(0, T):
+        A, B = map(int, input().split())
+        a, b = A, B
+        remainder = b % a
+        while not remainder == 0:
+            b = a
+            a = remainder
+            remainder = b % a
+        greatestCommonDivisor = a
+        leastCommonMultiple = int( (A*B)/a )
+        print(leastCommonMultiple)
+
+if __name__ == "__main__":
+    main()
