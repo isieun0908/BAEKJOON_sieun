554220239c0b48b3d01fef7c68af3c2c09954bee
Sieun <supiya0908@naver.com>
2022-04-01 23:43:11 +0900

    step 16 11051-이항 계수 2 (!not correct)

---
+++ Step 16/11051.py
@@ -0,0 +1,16 @@
+def main():
+    N, K = map(int, input().split())
+
+    n_fac = 1
+    for i in range(2, N+1):
+        n_fac *= i
+    k_fac = 1
+    for i in range(2, K+1):
+        k_fac *= i
+    n_k_fac = 1
+    for i in range(2, N-K+1):
+        n_k_fac *= i
+    print(n_fac / (k_fac * n_k_fac) % 10007)
+
+if __name__ == "__main__":
+    main()
