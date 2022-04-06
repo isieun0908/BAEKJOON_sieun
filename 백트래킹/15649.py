6e798bca163f71941cf47c6dbe13c4ff26beb31b
Sieun <supiya0908@naver.com>
2022-04-06 23:41:10 +0900

    step 13 15649-N과 M (1)

---
+++ 백트래킹/15649.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    M, N = map(int, input().split())
+
+    for m in range(0, M):
+        for n in range(0, N):
+            print(m, n)
+
+
+if __name__ == "__main__":
+    main()
