ec69bb4273f280f574dac0abccb500b5325cd3c6
Sieun <supiya0908@naver.com>
2022-04-08 23:54:35 +0900

    step 16 2981-검증

--- Step 16/2981.py
+++ Step 16/2981.py
@@ -1,25 +1,36 @@
 def main():
     N = int(input())
 
-    list = []
-    for i in range(0, N):
-        num = int(input())
-        list.append(num)
-
-    maxN = max(list)
     M = []
-    for i in range(2, maxN+1):
-        flag = 1
-        remainder = list[0] % i
-        for j in list:
-            if not j % i == remainder:
-                flag = 0
-                break
-        if flag:
-            M.append(i)
+    for n in range(0, N):
+        m = int(input())
+        M.append(m)
+    print(M)
 
-    for i in M:
-        print(i, end = ' ')
+    greatestCommonDivisor = 1
+
+    while True:
+        end = 0
+        minM = min(M)
+        #print("min", minM)
+        for i in range(2, minM+1):
+            #print(i)
+            flag = 0
+            for j in M:
+                if j % i == 0:
+                    flag += 1
+                    break
+            if flag == N-1:
+                print(i)
+                for n in range(0, N):
+                    M[n] = int(M[n] / i)
+                greatestCommonDivisor *= i
+                break
+            if flag == 0:
+                end = 1
+        if end == 1:
+            break
+    print("최대공약수 : ", greatestCommonDivisor)
 
 if __name__ == "__main__":
     main()
