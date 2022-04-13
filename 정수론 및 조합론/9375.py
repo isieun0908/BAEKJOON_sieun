3cd435ca5804168b913405ec1f45401f52ad2721
Sieun <supiya0908@naver.com>
2022-04-13 23:22:41 +0900

    step '정수론 및 조합론' 9375-패션왕 신해빈

--- 정수론 및 조합론/9375.py
+++ 정수론 및 조합론/9375.py
@@ -4,16 +4,22 @@ def main():
     for t in range(0, T):
         n = int(input())
 
+        # 입력받아 dictionary에 저장
         wear = {}
         for tc in range(0, n):
             wear_name, wear_type = input().split()
-
             if wear_type in wear:
                 wear[wear_type] += 1
             else:
                 wear[wear_type] = 1
 
-        print(wear)
+        # 경우의 수 세기
+        # [각 종류의 옷 + 1(선택X)]를 모두 곱한 후,
+        # 아무것도 안 입는 경우 -1하기
+        cases = 1
+        for case in wear.values():
+            cases *= case + 1
+        print(cases - 1)
 
 if __name__ == "__main__":
     main()
