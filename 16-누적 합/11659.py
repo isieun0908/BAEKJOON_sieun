2e8e909a34bfa0b58025e6c274e4e2e0b0cf387b
Sieun <supiya0908@naver.com>
2022-05-07 23:59:26 +0900

    step '누적 합' - 11659 (!modify)

--- 16-누적 합/11659.py
+++ 16-누적 합/11659.py
@@ -2,13 +2,18 @@ def main():
     N, M = map(int, input().split())
 
     numList = list(map(int, input().split()))
+    for n in range(1, N):       # 누적 합 리스트 생성
+        numList[n] += numList[n-1]
+    numList = [0]
 
-    for m in range(0, M):
-        i, j = map(int, input().split())
-        sum = 0
-        for index in range(i-1, j):
-            sum += numList[index]
-        print(sum)
+    for m in range(0, M):       # 테스트 케이스 M
+        i, j = map(int, input().split())    # 입력
+
+        index = i - 2
+        if index < 0:
+            print(numList[j-1])
+        else:
+            print(numList[j-1] - numList[index])
 
 if __name__ == "__main__":
     main()
