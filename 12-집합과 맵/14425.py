8b93365e86a9c6460b7578bfabeff57208a5e4d6
Sieun <supiya0908@naver.com>
2022-05-14 00:45:29 +0900

    step '집합과 맵' - 14425

--- 12-집합과 맵/14425.py
+++ 12-집합과 맵/14425.py
@@ -6,7 +6,12 @@ def main():
         inputData = input()
         S.append(inputData)
 
-    print(S)
+    count = 0
+    for i in range(0, M):
+        inputData = input()
+        if inputData in S:
+            count += 1
+    print(count)
 
 if __name__ == "__main__":
     main()
