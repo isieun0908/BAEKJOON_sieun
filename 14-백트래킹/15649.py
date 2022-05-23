1ed28b2fa9622e1d9ab385d7a25faa615954e09b
Sieun <supiya0908@naver.com>
2022-05-24 00:34:03 +0900

    step '백트래킹' - 15649 (!time over)

--- 14-백트래킹/15649.py
+++ 14-백트래킹/15649.py
@@ -1,7 +1,39 @@
 def main():
     N, M = map(int, input().split())
+    numList = []
+    for i in range(0, M):
+        numList.append(1)
 
-    s
+    while True:
+        # 출력
+        dupli = []
+        isDupli = 1
+        for i in range(0, M):
+            if numList[i] in dupli:
+                isDupli = 0
+                break
+            dupli.append(numList[i])
+
+        if isDupli:
+            for i in range(0, M):
+                print(numList[i], end=' ')
+            print()
+
+        # 종료
+        flag = 1
+        for i in range(0, M):
+            if not numList[i] == N:
+                flag = 0
+                break
+        if flag:
+            break
+
+        # 증가
+        numList[M-1] += 1
+        for i in range(M-1, -1, -1):
+            if numList[i] >= N + 1:
+                numList[i] = 1
+                numList[i-1] += 1
 
 if __name__ == "__main__":
     main()
