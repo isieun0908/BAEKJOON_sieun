bc9970833b856276272dd06e8a0a4010555cab63
Sieun <supiya0908@naver.com>
2022-05-25 23:59:27 +0900

    step '백트래킹' - 15650 (!time over)

---
+++ 14-백트래킹/15650.py
@@ -0,0 +1,11 @@
+def main():
+    N, M = map(int, input().split())
+    numList = []
+    for i in range(0, M):
+        numList.append(1)
+
+    while True:
+
+
+if __name__ == "__main__":
+    main()
