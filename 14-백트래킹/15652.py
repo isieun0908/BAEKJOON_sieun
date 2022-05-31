5668eef029ea15708aa2224f59f9289bcf7f81e8
Sieun <supiya0908@naver.com>
2022-06-01 00:00:11 +0900

    step '백트래킹' - 15652

---
+++ 14-백트래킹/15652.py
@@ -0,0 +1,30 @@
+def main():
+    N, M = map(int, input().split())
+    numList = []
+    for i in range(0, M):
+        numList.append(1)
+
+    while True:
+        # 출력
+        for i in range(0, M):
+            print(numList[i], end=' ')
+        print()
+
+        # 종료
+        flag = 1
+        for i in range(0, M):
+            if not numList[i] == N:
+                flag = 0
+                break
+        if flag:
+            break
+
+        # 증가
+        numList[M-1] += 1
+        for i in range(M-1, -1, -1):
+            if numList[i] >= N + 1:
+                numList[i-1] += 1
+                numList[i] = numList[i-1]
+
+if __name__ == "__main__":
+    main()
