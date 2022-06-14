469fcf29c0db2bc40c9a98a670fea357f5f2f560
Sieun <supiya0908@naver.com>
2022-06-14 23:14:07 +0900

    step '스택' - 4949(!not correct)

--- 19-스택/4949.py
+++ 19-스택/4949.py
@@ -1,31 +1,23 @@
 def main():
-    stackS = []
-    stackB = []
+    stack = []
     while True:
         flag = 1
-        stackS.clear()
-        stackB.clear()
+        stack.clear()
         inputData = input()
         if inputData == ".":
             break
         for i in inputData:
-            if i == "(":
-                stackS.append(1)
-            elif i == "[":
-                stackB.append(1)
-            elif i == ")":
-                if not len(stackS) == 0:
-                    stackS.pop()
+            if i == "(" or i == "[":
+                stack.append(i)
+            elif i == ")" or i == "]":
+                if len(stack) > 0:
+                    popData = stack.pop()
+                    if not (popData == "(" and i == ")") and not (popData == "[" and i == "]"):
+                        flag = 0
+                        break
                 else:
                     flag = 0
                     break
-            elif i == "]":
-                if not len(stackB) == 0:
-                    stackB.pop()
-                else:
-                    flag = 0
-                    break
-        print(flag)
         if flag == 1:
             print("yes")
         else:
