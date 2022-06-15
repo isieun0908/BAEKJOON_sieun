bdb73931532264ab95bc8b8788d1f3f553e8b0c6
Sieun <supiya0908@naver.com>
2022-06-15 23:56:39 +0900

    step '스택' - 1874(!not complete)

---
+++ 19-스택/1874.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+
+    stack = []
+    for i in range(n):
+        inputN = int(input())
+
+        if len(stack) == 0:
+            stack.append(inputN)
+            continue
+
+if __name__ == "__main__":
+    main()
