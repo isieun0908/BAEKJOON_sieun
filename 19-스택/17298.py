aeb59398ef1dee2536ec7665281853dad5aea589
Sieun <supiya0908@naver.com>
2022-06-21 00:08:00 +0900

    step '스택' - 17298(!time over)

--- 19-스택/17298.py
+++ 19-스택/17298.py
@@ -1,17 +1,16 @@
 def main():
     N = int(input())
-
-    A = []
-    for i in range(N):
-        inputData = int(input().split())
-        A.append(inputData)
-
+    A = list(map(int, input().split()))
     for i in range(N):
         a = A[i]
+        flag = 1
         for j in range(i, N):
             if a < A[j]:
+                flag = 0
                 print(A[j], end=' ')
-            break
+                break
+        if flag:
+            print(-1, end=' ')
 
 if __name__ == "__main__":
     main()
