4a8a791fc799e4f52d6a5b019826f1e8dc1092cd
Sieun <supiya0908@naver.com>
2022-06-24 19:01:53 +0900

    step '큐, 덱' - 18258(!not complete)

--- 18258.py
+++ 18258.py
@@ -1,5 +1,6 @@
 def push(x, stack):
     stack.append(x)
+    return stack
 
 def empty(stack):
     if len(stack) == 0:
@@ -13,32 +14,33 @@ def main():
 
     for i in range(N):
         inputData = input()
-
         try:
             command = inputData.split()
 
             if command[0] == "push":
-                push(command[1], stack)
+                stack = push(command[1], stack)
         except:
-            if inputData == "empty":
+            print("except")
+            if inputData == 'empty':
                 print(empty(stack))
-            elif inputData == "size":
+            elif inputData == 'size':
                 print(len(stack))
-            elif inputData == "front":
+            elif inputData == 'front':
                 if empty(stack) == 1:
                     print(-1)
                 else:
                     print(stack[0])
-            elif inputData == "back":
-                if empty(stack) == 1:
+            elif inputData == 'back':
+                if empty(stack):
                     print(-1)
                 else:
-                    print(stack[len(stack)-1])
-            elif inputData == "pop":
+                    print(len(stack), stack[len(stack)-1])
+            elif inputData == 'pop':
                 if empty(stack) == 1:
                     print(-1)
                 else:
                     print(stack.pop(0))
+        print(stack)
 
 if __name__ == "__main__":
     main()
