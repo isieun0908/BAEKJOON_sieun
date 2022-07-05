5f24ba1ec62ec70fa2acf7b0233c1b897e6d13bd
Sieun <supiya0908@naver.com>
2022-07-05 23:59:41 +0900

    step '큐, 덱' - 18258(!not complete)

--- 20-큐, 덱/18258.py
+++ 20-큐, 덱/18258.py
@@ -21,25 +21,28 @@ def main():
                 stack = push(command[1], stack)
         except:
             print("except")
-            if inputData == 'empty':
+            if inputData == "empty":
                 print(empty(stack))
-            elif inputData == 'size':
+            elif inputData == "size":
                 print(len(stack))
-            elif inputData == 'front':
+            elif inputData == "front":
+                print("~~")
                 if empty(stack) == 1:
                     print(-1)
                 else:
                     print(stack[0])
-            elif inputData == 'back':
+            elif inputData == "back":
                 if empty(stack):
                     print(-1)
                 else:
                     print(len(stack), stack[len(stack)-1])
-            elif inputData == 'pop':
+            elif inputData == "pop":
                 if empty(stack) == 1:
                     print(-1)
                 else:
                     print(stack.pop(0))
+            else:
+                print("else")
         print(stack)
 
 if __name__ == "__main__":
