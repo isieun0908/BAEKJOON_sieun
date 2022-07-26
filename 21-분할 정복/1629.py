9ae2226b95a589d8fb1c75280645d1a41b0af5ed
sieun <supiya0908@naver.com>
2022-07-27 04:06:30 +0900

    step '분할 정복'-1629(!time over)

--- 21-분할 정복/1629.py
+++ 21-분할 정복/1629.py
@@ -1,7 +1,12 @@
+import math
+
 def main():
     A, B, C = map(int, input().split())
-
-    print((A**B)%C)
+    half = math.floor(B/2)
+    if B % 2 ==0:   # 짝수
+        print(((A**half)**2)%C)
+    else:
+        print((((A**half)**2)*A)%C)
 
 if __name__ == "__main__":
     main()
