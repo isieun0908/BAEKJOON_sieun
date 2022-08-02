d4b8ec241ce177517d3d9da2962fcc704a3077a5
Sieun <supiya0908@naver.com>
2022-08-03 01:19:26 +0900

    step '분할 정복' - 11401(!time over)

--- 21-분할 정복/11401.py
+++ 21-분할 정복/11401.py
@@ -1,6 +1,18 @@
 def main():
-    N, K = map(int,input().split())
+    N, K = map(int, input().split())
 
-    print(N%1000000007)
+    binomialC = 1
+
+    if not K == 0 and not K == N:
+        numerator = N
+        denominator = 1
+        for i in range(1, K+1):
+            binomialC *= numerator
+            numerator -= 1
+            denominator *= i
+        binomialC /= denominator
+
+    print(int(binomialC % 1000000007))
+
 if __name__ == "__main__":
     main()
