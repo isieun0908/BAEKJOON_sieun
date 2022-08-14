be9b3931a27dcb590876e32a61b7fcffc5774847
Sieun <supiya0908@naver.com>
2022-08-14 23:58:29 +0900

    step '우선순위 큐' - 1655(!not correct)

---
+++ 23-우선순위 큐/1655.py
@@ -0,0 +1,34 @@
+from sys import stdin
+from bisect import bisect_left
+
+def pop(minHeap):
+    if len(minHeap) == 0:
+        print(0)
+        return minHeap
+    result = minHeap.pop(0)
+    print(result)
+    return minHeap
+
+
+def push(n, minHeap):
+    n = abs(n)
+    index = bisect_left(minHeap, n)
+    minHeap.insert(index, n)
+    return minHeap
+
+
+def main():
+    N = int(stdin.readline())
+    data = [int(stdin.readline().strip()) for i in range(N)]
+    minHeap = []
+
+    for x in data:
+        if x == 0:
+            minHeap = pop(minHeap)
+        else:
+            minHeap = push(x, minHeap)
+        print(minHeap[int(len(minHeap)/2)])
+
+
+if __name__ == "__main__":
+    main()
