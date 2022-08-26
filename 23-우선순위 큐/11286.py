3cb05af997188afbb408ab0c39d9cf948ea95ccd
Sieun <supiya0908@naver.com>
2022-08-26 23:26:33 +0900

    step 23 '우선순위 큐' - 11286(!not correct)

--- 23-우선순위 큐/11286.py
+++ 23-우선순위 큐/11286.py
@@ -1,30 +1,33 @@
 from sys import stdin
 from bisect import bisect_left
 
-def pop(minHeap):
-    if len(minHeap) == 0:
+def pop(absHeap, valueHeap):
+    if len(absHeap) == 0:
         print(0)
-        return minHeap
-    result = minHeap.pop(0)
+        return absHeap, valueHeap
+    result = valueHeap.pop(0)
+    absHeap.pop(0)
     print(result)
-    return minHeap
+    return absHeap, valueHeap
 
-def push(n, minHeap):
-    n = abs(n)
-    index = bisect_left(minHeap, n)
-    minHeap.insert(index, n)
-    return minHeap
+def push(n, absHeap, valueHeap):
+
+    index = bisect_left(absHeap, abs(n))
+    absHeap.insert(index, n)
+    valueHeap.insert(index, n)
+    return absHeap, valueHeap
 
 def main():
     N = int(stdin.readline())
     data = [int(stdin.readline().strip()) for i in range(N)]
-    minHeap = []
+    valueHeap = []
+    absHeap = []
     
     for x in data:
         if x == 0:
-            minHeap = pop(minHeap)
+            absHeap, valueHeap = pop(absHeap, valueHeap)
         else:
-            minHeap = push(x, minHeap)
+            absHeap, valueHeap = push(x, absHeap, valueHeap)
 
     # 절대값 고려해서 push or pop하되 뽑아서 출력할 때는 원본 값
 
