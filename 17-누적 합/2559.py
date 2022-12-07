506855447733900f1874511a34c3773fbdf21bf3
isieun0908 <supiya0908@naver.com>
2022-12-07 18:17:10 +0900

    Feat: 누적합 - 2559(success)

--- 17-누적 합/2559.py
+++ 17-누적 합/2559.py
@@ -1,13 +1,22 @@
+import sys
+
 def main():
-    N, K = map(int, input().split())
-    temperature = list(map(int, input().split()))
+    N, K = map(int, sys.stdin.readline().rstrip().split())
+    temperature = list(map(int, sys.stdin.readline().rstrip().split()))
 
-    max = -100
-    for i in range(K-1, N):
-        sumT = sum(temperature[i-(K-1):i+1])
-        if sumT > max:
-            max = sumT
+    sum_list = [0]
+    sum = 0
+    for i in temperature:
+        sum += i
+        sum_list.append(sum)
+    
+    max = -sys.maxsize
+    for i in range(K, N+1):
+        temperature_sum = sum_list[i] - sum_list[i-K]
+        if temperature_sum > max:
+            max = temperature_sum
+    
     print(max)
-
+    
 if __name__ == "__main__":
     main()
